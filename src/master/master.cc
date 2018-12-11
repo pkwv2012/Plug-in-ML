9dfa50f6be4cbea2f2fae9e927c666ed2b73cadc
Peikai Zheng <bnuzpk@gmail.com>
2018-12-11 11:35:49 +0800

    add master

---
+++ src/master/master.cc
@@ -0,0 +1,14 @@
+// Copyright 2018 The RPSCC Authors. All Rights Reserved.
+//
+// Created by PeikaiZheng on 2018/12/8.
+//
+
+#include "src/master/master.h"
+
+namespace rpscc {
+
+void Master::WaitForClusterReady() {
+}
+
+}
+
