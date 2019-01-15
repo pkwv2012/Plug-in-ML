bff20c968829c56b06f2e6b2bcbfcecdded677a9
Peikai Zheng <bnuzpk@gmail.com>
2019-01-15 13:57:47 +0800

    update master

---
+++ src/master/task_config_test.cc
@@ -0,0 +1,11 @@
+// Copyright 2018 The RPSCC Authors. All Rights Reserved.
+// Created by pkwv on 1/12/19.
+
+#include "gtest/gtest.h"
+#include "src/master/task_config.h"
+
+TEST(TaskConfig, Generator) {
+  TaskConfig task_config;
+
+}
+
