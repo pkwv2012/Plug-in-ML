14dbb9ae4341983e6a638123522050bbe04153bc
Peikai Zheng <bnuzpk@gmail.com>
2019-01-24 17:29:46 +0800

    fix fifo name & shm name

--- src/agent/agent_main.cc
+++ src/agent/agent_main.cc
@@ -9,10 +9,10 @@ DEFINE_string(master_ip_port, "", "The master's ip & port.");
 int main(int argc, char** argv) {
   gflags::ParseCommandLineFlags(&argc, &argv, false);
   rpscc::Agent* agent = new rpscc::Agent();
-  std::string para_fifo_name = "/dev/shm/test_sharedMemory_sample1";
-  std::string grad_fifo_name = "/dev/shm/test_sharedMemory_sample2";
-  std::string para_memory_name = "/tmp/test_fifo_sample1";
-  std::string grad_memory_name = "/tmp/test_fifo_sample2";
+  std::string para_memory_name = "/test_sharedMemory_sample1";
+  std::string grad_memory_name = "/test_sharedMemory_sample2";
+  std::string para_fifo_name = "/tmp/test_fifo_sample1";
+  std::string grad_fifo_name = "/tmp/test_fifo_sample2";
   
   agent->Initialize(para_fifo_name, grad_fifo_name, 
                     para_memory_name, grad_memory_name, 
