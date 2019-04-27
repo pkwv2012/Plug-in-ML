8a7e72b6da2fffa9f0a1f79c6e253a84df5ca4cc
Peikai Zheng <bnuzpk@gmail.com>
2019-04-27 18:43:13 +0800

    add signal handler

--- src/agent/agent_main.cc
+++ src/agent/agent_main.cc
@@ -1,12 +1,32 @@
 // Copyright 2018 The RPSCC Authors. All Rights Reserved.
 // Author : Chenbin Zhang (zcbin@pku.edu.cn)
 
+#include <execinfo.h>
+#include <signal.h>
+#include <unistd.h>
+
 #include "src/agent/agent.h"
 
 DEFINE_int32(listen_port, 5555, "The listening port of agent");
 DEFINE_string(master_ip_port, "", "The master's ip & port.");
 
+void SegHandler(int sig) {
+  void *array[10];
+  size_t size;
+
+  // get void*'s for all entries on the stack
+  size = backtrace(array, 10);
+
+  // print out all the frames to stderr
+  fprintf(stderr, "Error: signal %d:\n", sig);
+  backtrace_symbols_fd(array, size, STDERR_FILENO);
+  exit(1);
+}
+
 int main(int argc, char** argv) {
+  // 1. install signal handler
+  signal(SIGSEGV, SegHandler);
+  signal(SIGINT, SegHandler);
   gflags::ParseCommandLineFlags(&argc, &argv, false);
   rpscc::Agent* agent = new rpscc::Agent();
   std::string para_memory_name = "/test_sharedMemory_sample1";
--- src/server/server_main.cc
+++ src/server/server_main.cc
@@ -1,12 +1,32 @@
 // Copyright 2018 The RPSCC Authors. All Rights Reserved.
 // Author : Xu Song (sazel.sekibanki@gmail.com)
 
+#include <execinfo.h>
+#include <signal.h>
+#include "unistd.h"
+
 #include "gflags/gflags.h"
 #include "src/util/logging.h"
 #include "src/server/server.h"
 
+void SegHandler(int sig) {
+  void *array[10];
+  size_t size;
+
+  // get void*'s for all entries on the stack
+  size = backtrace(array, 10);
+
+  // print out all the frames to stderr
+  fprintf(stderr, "Error: signal %d:\n", sig);
+  backtrace_symbols_fd(array, size, STDERR_FILENO);
+  exit(1);
+}
+
 // server_main defines the main function for a server process.
 int main(int argc, char **argv) {
+  // 1. install signal handler
+  signal(SIGSEGV, SegHandler);
+  signal(SIGINT, SegHandler);
   gflags::ParseCommandLineFlags(&argc, &argv, true);
   rpscc::Server server;
   if (server.Initialize() == false) {
