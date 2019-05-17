bee803f2c6ba342aa7162f7cbec4678da7fab1e1
Peikai Zheng <bnuzpk@gmail.com>
2019-05-17 19:35:53 +0800

    fix install dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -8,6 +8,12 @@ make check
 sudo make install
 sudo ldconfig
 cd $HOME/.cache
-echo "deb http://download.opensuse.org/repositories/network:/messaging:/zeromq:/release-stable/Debian_9.0/ ./" >> /etc/apt/sources.list
-wget https://download.opensuse.org/repositories/network:/messaging:/zeromq:/release-stable/Debian_9.0/Release.key -O- | sudo apt-key add
-sudo apt-get install libzmq3-dev
+# Use another method to install libzmq
+# echo "deb http://download.opensuse.org/repositories/network:/messaging:/zeromq:/release-stable/Debian_9.0/ ./" >> /etc/apt/sources.list
+# wget https://download.opensuse.org/repositories/network:/messaging:/zeromq:/release-stable/Debian_9.0/Release.key -O- | sudo apt-key add
+# sudo apt-get install libzmq3-dev
+wget https://github.com/zeromq/libzmq/releases/download/v4.3.1/zeromq-4.3.1.tar.gz
+tar -xvf zeromq-4.3.1.tar.gz
+cd zeromq-4.3.1
+./autogen.sh && ./configure && make -j $(nproc)
+make check && sudo make install && sudo ldconfig
